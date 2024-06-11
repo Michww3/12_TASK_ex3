3999b914d30012214970356e4d5909fb822005df
User <User@W11PC>
2024-06-11 17:50:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    private static readonly string MutexName = "Global\\UniqueApplicationNameMutex";
+
+    static void Main()
+    {
+        using (Mutex mutex = new Mutex(false, MutexName, out bool isNewInstance))
+        {
+            if (!isNewInstance)
+            {
+                Console.WriteLine("Another instance of this application is already running. Exiting the application.");
+                //Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("This is the only instance of the application running.");
+            Console.ReadLine();
+        }
+    }
+}
